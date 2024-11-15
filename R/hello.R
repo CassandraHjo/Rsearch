b29179af998b0175a1feb0e5d622047451ceb6c3
Cassandra <132651748+CassandraHjo@users.noreply.github.com>
2024-11-15 13:37:00 +0100

    Added another toy function

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -15,4 +15,15 @@
 
 hello <- function() {
   print("Hello, world!")
+}
+
+#' Prints goodbye message
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' bye()
+bye <- function() {
+  print("Goodbye, world!")
 }
--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -1,3 +1,7 @@
 test_that("hello() prints Hello, world!", {
   expect_equal(hello(), c("Hello, world!"))
 })
+
+test_that("bye() prints Goodbye, world!", {
+  expect_equal(bye(), c("Goodbye, world!"))
+})
