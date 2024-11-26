665ad98fddd6da5775d3f7a7c430a90c65c55526
Cassandra <132651748+CassandraHjo@users.noreply.github.com>
2024-11-26 08:10:01 +0100

    delete tests for toy functions

--- tests/testthat/test-hello.R
+++
@@ -1,7 +0,0 @@
-test_that("hello() prints Hello, world!", {
-  expect_equal(hello(), c("Hello, world!"))
-})
-
-test_that("bye() prints Goodbye, world!", {
-  expect_equal(bye(), c("Goodbye, world!"))
-})
