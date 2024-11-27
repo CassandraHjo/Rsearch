96cdd85505eb27ea92414aa0d97fc555975161ff
Lars-Gustav Snipen <lars.snipen@nmbu.no>
2024-11-27 13:40:52 +0100

    Minor change to vsearch_available

--- R/vsearch.R
+++ R/vsearch.R
@@ -29,12 +29,11 @@ set_vsearch_executable <- function(vsearch_executable){
 
 # Non-exported function to gracefully fail when vsearch_executable
 # does not contains a proper command line
-vsearch_available <- function(){
+vsearch_available <- function(vsearch_executable){
   chr <- NULL
-  vsearch_executable <- options("Rsearch.vsearch_executable")[[1]]
   ok <- try(chr <- system2(vsearch_executable, stdout = TRUE), silent = TRUE)
   if(length(grep("Error", ok[1])) > 0){
-    stop("Cannot run ", vsearch_executable, " from R, use set_vsearch_executable()\nto set proper command to invoke vsearch")
+    stop("Cannot run ", vsearch_executable, " from R, use set_vsearch_executable() to set proper command to invoke vsearch")
     return(FALSE)
   } else {
     return(TRUE)
