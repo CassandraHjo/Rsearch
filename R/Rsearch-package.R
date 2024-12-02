e0929ec0ff822a21b8faec88c6a6b4e0920ff8cd
Cassandra <132651748+CassandraHjo@users.noreply.github.com>
2024-12-02 15:41:18 +0100

    update

---
+++ R/Rsearch-package.R
@@ -0,0 +1,9 @@
+#' Rsearch
+#'
+#' A brief description of your package's purpose and functionality.
+#'
+#' @keywords internal
+#' @importFrom magrittr %>%
+#' @importFrom Rcpp evalCpp
+#' @useDynLib Rsearch, .registration = TRUE
+"_PACKAGE"
