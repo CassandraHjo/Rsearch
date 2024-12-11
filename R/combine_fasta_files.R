17d51fb466820c44c421cd426ada541c2e5f23db
Cassandra <132651748+CassandraHjo@users.noreply.github.com>
2024-12-11 12:01:02 +0100

    now able to combine both fasta and fastq files. Only return tibble if file is not specified.

--- R/combine_fasta_files.R
+++ R/combine_fasta_files.R
@@ -1,65 +1,75 @@
-#' Combine FASTA files
+#' Combine content files in one directory
 #'
-#' @description Combines all FASTA-files in given directory into one FASTA object.
+#' @description Combines all files of given type in given directory into one file/object.
 #'
-#' @param fasta_dir Path to directory with FASTA files.
-#' @param output_file Name of output FASTA-file or \code{NULL}. If not specified, the function will only return a FASTA object (a tibble with the columns \code{Header} and \code{Sequence}).
+#' @param files_dir Path to directory with files to combine.
+#' @param output_file Name of output file or \code{NULL}. If not specified, the function will only return a FASTA/FASTQ object depending on \code{file_format}.
+#' @param file_ext The file extension for the files you want to combine. Must be written with a "." in front of the extension.
+#' @param file_format Format of files you want to combine, and desired output format: \code{"fasta"} or \code{"fastq"}.
 #'
-#' @return A FASTA object with the columns \code{Header} and \code{Sequence}. If \code{output_file} is specified the path to the resulting FASTA file will be an attribute (\code{file_path})of the FASTA object.
-#' This attribute can be accessed by running \code{attributes(all_fasta_tbl)$file_path} or \code{attr(all_fasta_tbl, "file_path")}.
+#' @return If \code{output_file} is not specified, a tibble containing the combined reads in the format specified in \code{file_format} is returned. If \code{output_file} is specified nothing is returned.
 #'
 #' @export
-combine_fasta_files <- function(fasta_dir, output_file = NULL) {
+combine_fasta_files <- function(files_dir, output_file = NULL, file_ext = ".fa", file_format = "fasta") {
 
   # Check if input directory exists
-  if (!dir.exists(fasta_dir)) {
-    stop("Directory does not exist: ", fasta_dir)
+  if (!dir.exists(files_dir)) {
+    stop("Directory does not exist: ", files_dir)
+  }
+
+  # Validate file_format
+  if (!file_format %in% c("fasta", "fastq")) {
+    stop("Invalid file_format. Choose from fasta or fastq.")
   }
 
   # Create empty vector for collecting temporary files
   temp_files <- c()
 
-  # Find all .fa files in the folder
-  fa_files <- list.files(fasta_dir, pattern = "\\.fa$", full.names = TRUE)
+  # Set up cleanup of temporary files
+  on.exit({
+    if (length(temp_files) > 0) {
+      file.remove(temp_files)
+    }
+  }, add = TRUE)
 
-  # Check if any .fa files are found
-  if (length(fa_files) == 0) {
-    stop("No .fa files found in the specified folder: ", fasta_dir)
+  # Find all files in the folder
+  comb_files <- list.files(files_dir, pattern = paste0("\\", file_ext, "$"), full.names = TRUE)
+
+  # Check if any files are found
+  if (length(comb_files) == 0) {
+    stop("No ", file_ext, " files found in the specified folder: ", files_dir)
   }
 
   # Handle output file if NULL
   if (is.null(output_file)) {
-    # Create temporary output file
-    temp_file_all_fasta <- tempfile(pattern = "all_fasta", fileext = ".fa")
-    temp_files <- c(temp_files, temp_file_all_fasta)
-    output_file <- temp_file_all_fasta
+    out_file <- tempfile(pattern = "all_fasta", fileext = file_ext)
+    temp_files <- c(temp_files, out_file)
+  } else {
+    out_file <- output_file
   }
 
   # Full path to the combined output file
-  all_fasta <- file.path(output_file)
+  out_file <- file.path(out_file)
 
-  # Remove output file if it already exists
-  if (file.exists(all_fasta)) {
-    file.remove(all_fasta)
+  # Combine content of all files
+  file.append(out_file, comb_files)
+
+  # Create tibble if output_file not specified
+  if (is.null(output_file)) {
+    # Create FASTA object
+    if (file_format == "fasta") {
+      all_seq_tbl <- microseq::readFasta(out_file)
+    }
+    # Create FASTQ object
+    if (file_format == "fastq") {
+      all_seq_tbl <- microseq::readFastq(out_file)
+    }
   }
 
-  # Combine content of all fasta files
-  file.append(all_fasta, fa_files)
-
-  # Create FASTA object
-  all_fasta_tbl <- microseq::readFasta(all_fasta)
-
-  # Create attribute if output file is specified
-  if (!is.null(output_file)) {
-    attr(all_fasta_tbl, "file_path") <- all_fasta
+  # Return results
+  if (is.null(output_file)) { # Return tibble
+    return(all_seq_tbl)
+  } else {
+    return(invisible(NULL)) # No return when output file is written
   }
-
-  # Cleanup temporary files
-  if (length(temp_files) > 0) {
-    on.exit(
-      file.remove(temp_files),
-      add = TRUE)
-  }
-
-  return(all_fasta_tbl)
 }
