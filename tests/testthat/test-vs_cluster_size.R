0d433d361ef33d0c64dbf714ae4557fea66a7548
Cassandra <132651748+CassandraHjo@users.noreply.github.com>
2024-12-18 14:40:10 +0100

    add tests and test data for clustering

--- tests/testthat/test-vs_cluster_size.R
+++ tests/testthat/test-vs_cluster_size.R
@@ -5,3 +5,55 @@ test_that("error when input fasta_input does not exist", {
   expect_error(vs_cluster_size(fasta_input = fasta_input),
                paste("Cannot find input file:", fasta_input))
 })
+
+test_that("cluster sequences from fasta file, and return fasta file", {
+
+  fasta_input <- test_path("testdata", "sample1", "R1_sample1.fa")
+  centroids <- withr::local_tempfile()
+
+  return_value <- vs_cluster_size(fasta_input = fasta_input,
+                                  centroids = centroids)
+
+  expect_null(return_value)
+
+  expect_equal(microseq::readFasta(centroids),
+               microseq::readFasta(test_path("testdata", "output", "cluster_R1_sample1_file.fa")))
+})
+
+test_that("cluster sequences from fasta file, and return fasta tibble", {
+
+  fasta_input <- test_path("testdata", "sample1", "R1_sample1.fa")
+  centroids <- NULL
+
+  cluster_sample1_R1 <- vs_cluster_size(fasta_input = fasta_input,
+                                        centroids = centroids)
+
+  expect_equal(cluster_sample1_R1,
+               readRDS(test_path("testdata", "output", "cluster_R1_sample1_file.rds")))
+})
+
+test_that("cluster sequences from fasta tibble, and return fasta file", {
+
+  fasta_input <- microseq::readFasta(test_path("testdata", "sample1", "R1_sample1.fa"))
+  centroids <- withr::local_tempfile()
+
+  return_value <- vs_cluster_size(fasta_input = fasta_input,
+                                  centroids = centroids)
+
+  expect_null(return_value)
+
+  expect_equal(microseq::readFasta(centroids),
+               microseq::readFasta(test_path("testdata", "output", "cluster_R1_sample1_file.fa")))
+})
+
+test_that("cluster sequences from fasta tibble, and return fasta tibble", {
+
+  fasta_input <- microseq::readFasta(test_path("testdata", "sample1", "R1_sample1.fa"))
+  centroids <- NULL
+
+  cluster_sample1_R1 <- vs_cluster_size(fasta_input = fasta_input,
+                                        centroids = centroids)
+
+  expect_equal(cluster_sample1_R1,
+               readRDS(test_path("testdata", "output", "cluster_R1_sample1_tibble.rds")))
+})
