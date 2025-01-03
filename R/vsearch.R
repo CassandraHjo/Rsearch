99fd83b4ef77f1f87034267d6338c4f93cf78399
Lars-Gustav Snipen <lars.snipen@nmbu.no>
2025-01-03 14:25:02 +0100

    Added the small function vsearch(). This will only test if the vsearch_executable is working. It goes together with set_vsearch_executable()

--- R/vsearch.R
+++ R/vsearch.R
@@ -10,15 +10,16 @@
 #'
 #' If you have a windows computer and have copied the binary \code{vsearch.exe}
 #' to the folder C:/Documents/ on your computer, you update R with this
-#' information by  \code{set_vsearch_executable("C:/Documents/vsearch")}.
+#' information by  \code{set_vsearch_executable("C:/Documents/vsearch")}. Note that
+#' in R we use the \code{/} and not the \code{\} to separate folders as in Windows.
 #'
-#' If you are running on a HPC using apptainer containers you update by
-#' \code{set_vsearch_executable("apptainer exec <containername> vsearch")}.
+#' You may use the function \code{\link{vsearch}} to test if the command is valid.
 #'
 #' @return Nothing is returned, but the option \code{Rsearch.vsearch_executable}
 #' is updated. The string is also saved to a file for later R sessions, i.e. you
 #' only need to update this once (or if you change how you run/install VSEARCH).
 #'
+#' @seealso \code{\link{vsearch}}.
 #' @export
 #'
 set_vsearch_executable <- function(vsearch_executable){
@@ -27,8 +28,29 @@ set_vsearch_executable <- function(vsearch_executable){
 }
 
 
+#' Test if VSEARCH can be executed
+#'
+#' @description Tests if the VSEARCH executable is a valid command.
+#'
+#' @details Use this function to test the command used to invoke the external
+#' software VSEARCH on this computer.
+#'
+#' @seealso \code{\link{set_vsearch_executable}}.
+#'
+#' @export
+#'
+vsearch <- function(){
+  vsearch_executable <- options("Rsearch.vsearch_executable")[[1]]
+  cat("The VSEARCH executable is:", vsearch_executable, "\n")
+  ok <- vsearch_available(vsearch_executable)
+  if(ok){
+    cat("This is a valid command to invoke VSEARCH on this computer!\n")
+  }
+}
+
+
 # Non-exported function to gracefully fail when vsearch_executable
-# does not contains a proper command line
+# does not contain a proper command line
 vsearch_available <- function(vsearch_executable){
   chr <- NULL
   ok <- try(chr <- system2(vsearch_executable, args = c("-h", "--quiet", ""), stdout = TRUE), silent = TRUE)
