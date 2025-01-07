5a05af94f2c405907f7558287a4632e540640c72
Cassandra <132651748+CassandraHjo@users.noreply.github.com>
2025-01-07 14:55:51 +0100

    update and add decription from readme file

--- R/Rsearch-package.R
+++ R/Rsearch-package.R
@@ -1,6 +1,13 @@
 #' Rsearch
 #'
-#' A brief description of your package's purpose and functionality.
+#' An R package designed for handling and analyzing targeted sequencing data.
+#' Many of its functions are based on the command-line based tool VSEARCH.
+#' By integrating VSEARCH into an R package, Rsearch provides significant utility
+#' by making the software more accessible to a broad user base,
+#' particularly those who prefer or require an R-based workflow. In addition to
+#' the VSEARCH-based functions, Rsearch includes additional tools to aid
+#' in microbiome data analysis, such as data processing functions and visualizations
+#' of some of the results from the functions.
 #'
 #' @keywords internal
 #' @importFrom magrittr %>%
