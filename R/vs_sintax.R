1d5518a9795d3eb83df5d74ed65a1a15fc1bc022
Lars-Gustav Snipen <lars.snipen@nmbu.no>
2025-03-26 16:57:21 +0100

    Changed vs_sintax to return a table with taxonomy information in addition the Header and Sequence of every sequence classified.

--- R/vs_sintax.R
+++ R/vs_sintax.R
@@ -4,19 +4,19 @@
 #' implemented in \code{VSEARCH}.
 #'
 #' @param fasta_input A FASTA file path or a FASTA object with reads to
-#' classify. See \emph{Details}.
-#' @param db A FASTA/FASTQ file path or a FASTA/FASTQ object containing the
-#' reference database in FASTA/FASTQ format. The sequences need to be annotated
-#' with taxonomy.
-#' @param tabbedout Name of the tab-separated output file. If \code{NULL}
-#' (default), results are returned as a tibble. See \emph{Details}.
-#' @param cutoff minimum level of bootstrap support for the taxonomic ranks that
-#' is included in column 4 of the output file. Defaults to \code{NULL}.
+#' classify, see \emph{Details}.
+#' @param database A FASTA file path or a FASTA object containing the
+#' reference database in FASTA format. The sequences need to be annotated
+#' with taxonomy, see \emph{Details}.
+#' @param outfile Name of the output file. If \code{NULL}
+#' (default), results are returned as a data.frame.
+#' @param cutoff minimum level of bootstrap support (0.0-1.0) for the classifications.
+#' Defaults to \code{0.0}.
 #' @param strand Specifies which strand to consider when comparing sequences.
 #' Can be either \code{"plus"} (default) or \code{"both"}.
 #' @param randseed Seed for the random number generator used in the Sintax
 #' algorithm. Defaults to \code{NULL}.
-#' @param log_file Name of the log file to capture messages from \code{VSEARCH}.
+#' @param logfile Name of the log file to capture messages from \code{VSEARCH}.
 #' If \code{NULL} (default), no log file is created.
 #' @param threads Number of computational threads to be used by \code{VSEARCH}.
 #' Defaults to \code{1}.
@@ -24,48 +24,31 @@
 #' \code{VSEARCH}. Defaults to \code{NULL}. See \emph{Details}.
 #'
 #' @details The sequences in the input file are classified according to the
-#' Sintax algorithm, using \code{VSEARCH}.
+#' Sintax algorithm, using \code{VSEARCH}, see https://www.biorxiv.org/content/10.1101/074161v1
 #'
 #' \code{fasta_input} can either be a file path to a FASTA file or a
-#' FASTA object. \code{db} can either be a file path to a FASTA/FASTQ file or
-#' FASTA/FASTQ object. FASTA objects are tibbles that contain the
-#' columns \code{Header} and \code{Sequence}.FASTQ objects are tibbles that
-#' contain the columns \code{Header}, \code{Sequence}, and \code{Quality}.
+#' FASTA object.
 #'
-#' \code{tabbedout} is a tab-separated text format file with the following
-#' columns:
-#' \itemize{
-#'   \item \code{Query label}
-#'   \item \code{Predicted taxonomy}: The predicted taxonomy in the same format
-#'   as for the reference data, with bootstrap support indicated in parentheses
-#'   after each rank.
-#'   \item \code{Strand}
-#'   \item \code{Predicted toxonomy without bootstrap values, including only the
-#'   ranks with support at or above the threshold}: (only applies if
-#'   \code{cutoff} is specified)
-#' }
+#' \code{database} can either be a file path to a FASTA file or a
+#' FASTA object. FASTA objects are tibbles that contain the
+#' text columns \code{Header} and \code{Sequence}, see \code{\link{readFasta}}.
+#' The \code{Header} texts of this file must follow the sintax-pattern, see
+#' \code{\link{make_sintax_database}}.
 #'
 #' \code{vsearch_options} allows users to pass additional command-line arguments
 #' to \code{VSEARCH} that are not directly supported by this function. Refer to
 #' the \code{VSEARCH} manual for more details.
 #'
-#' @returns A tibble or \code{NULL}.
+#' @returns If \code{outfile} is \code{NULL} a data.frame is returned. If it contains
+#' a file name (text) the data.frame is written to that file with tab-separated columns.
 #'
-#' If \code{tabbedout} is \code{NULL}, a tibble containing the classification
-#' results with the following columns is returned:
-#' \itemize{
-#'     \item \code{query}: The query sequence identifier.
-#'     \item \code{predicted_taxonomy}: The predicted taxonomy with bootstrap
-#'     support.
-#'     \item \code{strand}: The strand where the match was found (\code{"plus"}
-#'     or \code{"minus"}).
-#'     \item \code{predicted_taxonomy_cutoff}: (If \code{cutoff} is specified)
-#'     The predicted taxonomy without bootstrap values, including only ranks
-#'     meeting the cutoff threshold.
-#'   }
-#'
-#'  If \code{tabbedout} is specified, results are written directly to the
-#'  specified file, and no tibble is returned.
+#' The data.frame contains the classification results for each input sequence.
+#' Both the \code{Header} and \code{Sequence} columns of \code{input_fasta} are
+#' copied into this table, and in addition are also the columns for each rank. The ranks
+#' depend on the database file used, but are typically domain, phylum, class, order,
+#' family, genus and species. For each classification is also a bootstrap support score.
+#' These are in separate columns with corresponding names, i.e. domain_score,
+#' phylum_score, etc.
 #'
 #' @references \url{https://github.com/torognes/vsearch}
 #'
@@ -74,12 +57,12 @@
 #' @export
 #'
 vs_sintax <- function(fasta_input,
-                      db,
-                      tabbedout = NULL,
+                      database,
+                      outfile = NULL,
                       cutoff = NULL,
                       strand = "plus",
                       randseed = NULL,
-                      log_file = NULL,
+                      logfile = NULL,
                       threads = 1,
                       vsearch_options = NULL){
 
@@ -119,66 +102,46 @@ vs_sintax <- function(fasta_input,
 
     # Capture original name for statistics table later
     fasta_input_name <- basename(fasta_input)
+    fasta_input <- microseq::readFasta(fasta_file)
   }
 
-  # Check is input file exists at given path
+  # Check if input file exists at given path
   if (!file.exists(fasta_file)) stop("Cannot find input file: ", fasta_file)
 
   # Handle input data base sequences
-  if (!is.character(db)){
-    if ("Quality" %in% colnames(db)){
-
-      # Validate tibble
-      required_cols <- c("Header", "Sequence", "Quality")
-      if (!all(required_cols %in% colnames(db))) {
-        stop("FASTQ data base must contain columns: Header, Sequence, Quality")
-      }
-
-      temp_file_db <- tempfile(pattern = "db_input", fileext = ".fq")
-      temp_files <- c(temp_files, temp_file_db)
-      microseq::writeFastq(db, temp_file_db)
-
-      db_file <- temp_file_db
-
-    } else {
-
-      # Validate tibble
-      required_cols <- c("Header", "Sequence")
-      if (!all(required_cols %in% colnames(db))) {
-        stop("FASTA data base must contain columns: Header and Sequence")
-      }
-
-      temp_file_db <- tempfile(pattern = "db_input", fileext = ".fa")
-      temp_files <- c(temp_files, temp_file_db)
-      microseq::writeFasta(db, temp_file_db)
-
-      db_file <- temp_file_db
-
+  if (!is.character(database)){
+    # Validate tibble
+    required_cols <- c("Header", "Sequence")
+    if (!all(required_cols %in% colnames(database))) {
+      stop("FASTA data base must contain columns: Header and Sequence")
     }
-  } else {
-    if (!file.exists(db)) stop("Cannot find input file: ", db)
 
-    db_file <- db
+    temp_file_db <- tempfile(pattern = "db_input", fileext = ".fa")
+    temp_files <- c(temp_files, temp_file_db)
+    microseq::writeFasta(db, temp_file_db)
+
+    db_file <- temp_file_db
+
+  } else {
+    if (!file.exists(database)) stop("Cannot find input file: ", database)
+
+    db_file <- database
   }
 
   # Normalize file paths
   fasta_file <- normalizePath(fasta_file)
   db_file <- normalizePath(db_file)
 
-  # Determine tabbedout file
-  if (is.null(tabbedout)) {
-    outfile <- tempfile(pattern = "output", fileext = ".txt")
-    temp_files <- c(temp_files, outfile)
-  } else {
-    outfile <- tabbedout
-  }
+  # The temporary outfile
+  tmp_outfile <- tempfile(pattern = "tmp_output", fileext = ".txt")
+  temp_files <- c(temp_files, tmp_outfile)
 
   # Build argument string for command line
   args <- c("--sintax", shQuote(fasta_file),
             "--db", shQuote(db_file),
             "--threads", threads,
             "--strand", strand,
-            "--tabbedout", outfile)
+            "--tabbedout", tmp_outfile)
 
   # Add additional arguments if specified
   if (!is.null(vsearch_options)) {
@@ -186,8 +149,8 @@ vs_sintax <- function(fasta_input,
   }
 
   # Add log file if specified
-  if (!is.null(log_file)){
-    args <- c(args, "--log", log_file)
+  if (!is.null(logfile)){
+    args <- c(args, "--log", logfile)
   }
 
   # Add cutoff if specified
@@ -206,23 +169,47 @@ vs_sintax <- function(fasta_input,
                             stdout = TRUE,
                             stderr = TRUE)
 
-  if (is.null(tabbedout)) {
+  # The output table
+  out.tbl <- fasta_input
+  out.tbl <- utils::read.table(tmp_outfile, sep = "\t", col.names = c("Header", "taxonomy", "plus")) |>
+    dplyr::select(-plus) |>
+    dplyr::mutate(domain = stringr::str_extract(taxonomy, "d:.+?\\)")) |>
+    dplyr::mutate(domain_score = as.numeric(stringr::str_remove_all(stringr::str_extract(domain, "\\(.+\\)"), "\\(|\\)"))) |>
+    dplyr::mutate(domain = stringr::str_remove_all(domain, "d:|\\(.+")) |>
+    dplyr::mutate(phylum = stringr::str_extract(taxonomy, "p:.+?\\)")) |>
+    dplyr::mutate(phylum_score = as.numeric(stringr::str_remove_all(stringr::str_extract(phylum, "\\(.+\\)"), "\\(|\\)"))) |>
+    dplyr::mutate(phylum = stringr::str_remove_all(phylum, "p:|\\(.+")) |>
+    dplyr::mutate(class = stringr::str_extract(taxonomy, "c:.+?\\)")) |>
+    dplyr::mutate(class_score = as.numeric(stringr::str_remove_all(stringr::str_extract(class, "\\(.+\\)"), "\\(|\\)"))) |>
+    dplyr::mutate(class = stringr::str_remove_all(class, "c:|\\(.+")) |>
+    dplyr::mutate(order = stringr::str_extract(taxonomy, "o:.+?\\)")) |>
+    dplyr::mutate(order_score = as.numeric(stringr::str_remove_all(stringr::str_extract(order, "\\(.+\\)"), "\\(|\\)"))) |>
+    dplyr::mutate(order = stringr::str_remove_all(order, "o:|\\(.+")) |>
+    dplyr::mutate(family = stringr::str_extract(taxonomy, "f:.+?\\)")) |>
+    dplyr::mutate(family_score = as.numeric(stringr::str_remove_all(stringr::str_extract(family, "\\(.+\\)"), "\\(|\\)"))) |>
+    dplyr::mutate(family = stringr::str_remove_all(family, "f:|\\(.+")) |>
+    dplyr::mutate(genus = stringr::str_extract(taxonomy, "g:.+?\\)")) |>
+    dplyr::mutate(genus_score = as.numeric(stringr::str_remove_all(stringr::str_extract(genus, "\\(.+\\)"), "\\(|\\)"))) |>
+    dplyr::mutate(genus = stringr::str_remove_all(genus, "g:|\\(.+")) |>
+    dplyr::mutate(species = stringr::str_extract(taxonomy, "s:.+?\\)")) |>
+    dplyr::mutate(species_score = as.numeric(stringr::str_remove_all(stringr::str_extract(species, "\\(.+\\)"), "\\(|\\)"))) |>
+    dplyr::mutate(species = stringr::str_remove_all(species, "s:|\\(.+")) |>
+    dplyr::select(-taxonomy) |>
+    dplyr::right_join(out.tbl, by = "Header") |>
+    dplyr::relocate(Sequence, .after = tidyr::last_col())
 
-    outfile_colnames <- c("query", "predicted_taxonomy", "strand")
-
-    if(!is.null(cutoff)){
-      outfile_colnames <- c(outfile_colnames, "predicted_taxonomy_cutoff")
-    }
+  # Write out.tbl to file
+  if (!is.null(outfile)) {
 
     # Read output into R tibble
-    tabbedout.tbl <- utils::read.delim(outfile,
-                                       header = FALSE,
-                                       col.names = outfile_colnames)
+    utils::write.table(out.tbl,
+                       file = outfile,
+                       sep = "\t")
   }
 
   # Return results
-  if (is.null(tabbedout)) { # Return tibble
-    return(tabbedout.tbl)
+  if (is.null(outfile)) { # Return tibble
+    return(out.tbl)
   } else {
     return(invisible(NULL)) # No return when output file is written
   }
