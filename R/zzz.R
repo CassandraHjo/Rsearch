a7d24eb329caa4d86666de7b863a3524305f8fbe
Cassandra <132651748+CassandraHjo@users.noreply.github.com>
2025-04-15 11:17:34 +0200

    updated .Rbuildignore and zzz.R in order to silence the NOTEs in check()

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,15 @@
   load(system.file("extdata/vsearch_executable.rds", package = "Rsearch"))
   options(Rsearch.vsearch_executable = vsearch_executable)
 }
+
+utils::globalVariables(c(
+  "vsearch_executable", "Header", "centroid_size", "tag", "domain", "phylum",
+  "family", "genus", "species", "Lower", "Upper", "MedianQuality", "MeanQuality",
+  "EE_rate", "Sequence", "Length", "Quality", "size", "num_reads",
+  "domain_score", "phylum_score", "class_score", "order_score", "family_score",
+  "genus_score", "species_score", "tax.tbl", "length_1", "length_2",
+  "length_merged", "length_overlap", "merged_read_pairs", "R1_length",
+  "R2_length", "truncee_rate_value", "value", "metric", "truncqual_value", "db",
+  "plus", "taxonomy", ".data"
+))
+
