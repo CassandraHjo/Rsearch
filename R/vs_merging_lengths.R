aa46442d0806a7e8e2dce4e9a50a75da7cc2f03c
Cassandra <132651748+CassandraHjo@users.noreply.github.com>
2025-08-27 12:43:49 +0200

    updated vs_merging_lengths with regex. updated tutorial, and built the site

--- R/vs_merging_lengths.R
+++ R/vs_merging_lengths.R
@@ -136,18 +136,18 @@ vs_merging_lengths <- function(fastq_input,
   # The lengths
   res.tbl <- R1.tbl |>
     dplyr::mutate(tag = stringr::str_extract(Header, "^\\S+")) |>
-    dplyr::mutate(tag = stringr::str_remove(tag, "/1$")) |>
+    dplyr::mutate(tag = stringr::str_remove(tag, "/[0-9]+")) |>
     dplyr::mutate(length_1 = stringr::str_length(Sequence)) |>
     dplyr::select(tag, length_1)
   res.tbl <- R2.tbl |>
     dplyr::mutate(tag = stringr::str_extract(Header, "^\\S+")) |>
-    dplyr::mutate(tag = stringr::str_remove(tag, "/2$")) |>
+    dplyr::mutate(tag = stringr::str_remove(tag, "/[0-9]+")) |>
     dplyr::mutate(length_2 = stringr::str_length(Sequence)) |>
     dplyr::select(tag, length_2) |>
     dplyr::full_join(res.tbl, by = "tag")
   res.tbl <- merged.tbl |>
     dplyr::mutate(tag = stringr::str_extract(Header, "^\\S+")) |>
-    dplyr::mutate(tag = stringr::str_remove(tag, "/1$")) |>
+    dplyr::mutate(tag = stringr::str_remove(tag, "/[0-9]+")) |>
     dplyr::mutate(length_merged = stringr::str_length(Sequence)) |>
     dplyr::select(tag, length_merged) |>
     dplyr::full_join(res.tbl, by = "tag") |>
